f92632deb6c98c0818d88b0d309981dfc86ae999
SebastiaNNNN <40791232+SebastiaNNNN@users.noreply.github.com>
2022-03-31 22:05:01 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 
 export default {
-  hostPassword: process.env.HOST_PASSWORD || "1234231321322",
+  hostPassword: process.env.HOST_PASSWORD || "1",
   maxDelay: parseInt(process.env.MAX_DELAY!) || 5000,
   clientVersionRequirements: "0.4.x",
   clientRecommendedVersion: "0.4.x"
